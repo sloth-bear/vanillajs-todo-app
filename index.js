0a280319ec52ba9f059e27085769dbc2bc976f64
sloth-bear <slothbear.hj@gmail.com>
2022-05-22 22:35:46 +0900

    Fix image path

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const images = ['1.jpg', '2.jpg', '3.jpg'];
+const images = ['https://user-images.githubusercontent.com/62458327/169697807-b4f2be90-eb8a-4cf2-bfcb-d797afc2f2a8.JPG', 'https://user-images.githubusercontent.com/62458327/169697814-c06e27ef-123c-4aa3-a238-a9606733f5e5.JPG', 'https://user-images.githubusercontent.com/62458327/169697817-6235a176-027d-4c1b-8a54-a64255dd883f.JPG'];
 
 function setBackgroundImage() {
     const randomIndex = Math.floor(Math.random() * images.length);
-    const randomImage = `https://github.com/sloth-bear/vanillajs-todo-app/blob/master/images/${images[randomIndex]}`;
-    
     const container = document.querySelector(".container");
-    container.style.backgroundImage = `url(${randomImage})`;
+    container.style.backgroundImage = `url(${images[randomIndex]})`;
 }
 
 window.addEventListener('load', () => {
